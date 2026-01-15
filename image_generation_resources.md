b043b4331e9c233957b8745d0ccddc3e67897db9
Mojtaba Peyrovi <mojtaba.peyrovi@gmail.com>
2026-01-15 19:37:47 +0100

    day four notes added

---
+++ image_generation_resources.md
@@ -0,0 +1,10 @@
+https://www.comfy.org/
+
+https://www.floyo.ai/
+
+
+https://www.runpod.io/
+
+https://vast.ai/
+
+
---
+++ practice1.py
@@ -0,0 +1,79 @@
+import openai
+import os
+import IPython
+from dotenv import load_dotenv
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+client = openai.OpenAI(
+    api_key = openai.api_key
+)
+
+
+
+# simple call
+response = client.responses.create(
+    model="gpt-4o-mini",
+    input="Write a one-sentence bedtime story about a unicorn."
+)
+
+print(response.output_text)
+
+
+
+# we can pass also the developer role or system prompt as instructions
+response = client.responses.create(
+    model="gpt-5",
+    reasoning={"effort": "low"},
+    instructions="Talk like a pirate.",
+    input="Are semicolons optional in JavaScript?",
+)
+
+
+print(response.output_text)
+
+
+# traditional developer role
+from openai import OpenAI
+client = OpenAI()
+
+response = client.responses.create(
+    model="gpt-5",
+    reasoning={"effort": "low"},
+    input=[
+        {
+            "role": "developer",
+            "content": "Talk like a pirate."
+        },
+        {
+            "role": "user",
+            "content": "Are semicolons optional in JavaScript?"
+        }
+    ]
+)
+
+print(response.output_text)
+
+
+
+
+# we can parameterize the prompts using variables
+
+# example:
+
+from openai import OpenAI
+client = OpenAI()
+
+response = client.responses.create(
+    model="gpt-5",
+    prompt={
+        "id": "pmpt_abc123",
+        "version": "2",
+        "variables": {
+            "customer_name": "Jane Doe",
+            "product": "40oz juice box"
+        }
+    }
+)
+
+print(response.output_text)
